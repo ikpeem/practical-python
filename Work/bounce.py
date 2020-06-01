d9f0b87c11bd860d53c499e5051458951bc0229a
ikpeem <lewis.ikpeme@gmail.com>
2020-06-01 21:10:59 +0100

    Chapter 1.2
    Completed chapter 1.2 of python course

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+#A ball is dropped from a height of 100 metres, each bounce is 3/5 of previous height
+#Print first ten bounces
+
+height = 100
+
+for i in range(10):
+    height *= 0.6
+    print(round(height, 4))
