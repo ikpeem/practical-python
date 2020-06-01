a34b68adb4151bafb8221c3f0b1c2a4ee8390622
ikpeem <lewis.ikpeme@gmail.com>
2020-06-01 21:46:45 +0100

    Chapter 1.3
    Completed chapter 1.3 of the python course

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
-#
-# Exercise 1.7
+#program that simulates a mortgage payment
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+
+months = 0
+
+extraPaymentStart = 60
+extraPaymentEnd = 108
+extraPayment = 1000
+
+while principal > 0:
+    if months > extraPaymentStart and months < extraPaymentEnd:
+        principal = principal * (1 + rate / 12) - (payment + extraPayment)
+        total_paid = total_paid + (payment + extraPayment)
+        months += 1
+    else:
+        principal = principal * (1 + rate / 12) - (payment)
+        total_paid = total_paid + (payment)
+        months += 1
+
+    if principal < 0:
+        principal = 0
+
+    print(months, ' ', principal)
+
+print('Total paid', round(total_paid, 2), 'Total months', months)
